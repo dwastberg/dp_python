a3f2d436484762b8d67e47d54d67ec8481ae2e47
Ben Miller-Jacobson <ben@catalonia.binha.us>
2015-07-22 13:31:38 -0400

    Fixes needed to compile under windows+anaconda at least.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup, Extension
+import numpy
 
 module1 = Extension('_dpcore_py',
                     sources = ['dpcore_py.c'])
@@ -6,4 +7,5 @@ module1 = Extension('_dpcore_py',
 setup (name = '_dpcore_py',
        version = '0.0',
        description = 'Dynamic programming core routine',
-       ext_modules = [module1])
+       ext_modules = [module1],
+       include_dirs = [numpy.get_include()])
